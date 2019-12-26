8ad78b704d207d27ce0d52107fdffe2e9d47d0f3
Wilyam <WilyamNosov@users.noreply.github.com>
2019-12-26 10:28:39 +0200

    add tests

---
+++ JustNote.Tests/NoteServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using JustNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using JustNote.Serivces;
+using Moq;
+
+namespace JustNote.Tests
+{
+    public class NoteServiceTests
+    {
+        [Fact]
+        public async void TestGet() 
+        {
+            var id = "1576154839832";
+            Mock<IDatabaseItemService<Note>> mock = new Mock<IDatabaseItemService<Note>>();
+
+            mock.Setup(noteService => noteService.Get(id)).ReturnsAsync(new Note());
+            var target = mock.Object;
+
+            var result = await target.Get(id);
+
+            Assert.Equal(new Note(), result);
+
+        }
+    }
+}
